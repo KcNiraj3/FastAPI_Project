2f6cd4a786463c3e5196ec9fe6fcc8599058bfb4
Niraj <kcniraj44@gmail.com>
2025-05-26 14:26:31 -0500

    commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from unittest.mock import Base
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import Column, String, Boolean
+
+
+
+
  
 app = FastAPI() 
 
@@ -7,12 +13,22 @@ app.add_middleware(
     CORSMiddleware
 )
 
+database_url = "sqlite.////"
+
 # #path parameter
 # @app.get("/{name}")
 # def index(name:str):
 #     return {"message":f"Hello World !, {name}"}
 
-# # query parameter - day & phone
-# @app.get("/{name}")
-# def index(name:str, day:str, phone:str):
-#     return(f"{name}, {day}, {phone}")
+# query parameter - day & phone
+@app.get("/{name}")
+def index(name:str, day:str, phone:str):
+    return(f"{name}, {day}, {phone}")
+
+class Task(Base):
+    __table__ = "tasks"
+    id = Column(String)
+    title = Column(String)
+    description = Column(String)
+    is_completed = Column(Boolean)
+
